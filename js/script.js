5eaf6cb19f1f12fc019c256c91ab6685b1f872b4
Cristian Castro <castrostech@gmail.com>
2023-10-12 18:47:58 -0500

    job-exp-bb

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 const toTopButton = document.querySelector("#to-top-button");
 
-
 nav.classList.add('hidden');
 
 btn.addEventListener('click', function() {
@@ -16,6 +15,10 @@ btn.addEventListener('click', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.toggle-button');
+
+       // Apply the active class to the first button on page load
+    // buttons[0].classList.add('active-work-title');
+
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             // Hide all content divs
@@ -28,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 btn.classList.remove('active-work-title');
             });
  
-
             // Show the clicked content
             const targetId = this.getAttribute('data-target');
             const targetContent = document.getElementById(targetId);
@@ -38,30 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// document.querySelectorAll(".toggle-button").forEach(btn => {
-//     btn.addEventListener("click", (e) => {
-//         const targetId = e.currentTarget.getAttribute("data-target");
-//         document.getElementById(targetId).classList.toggle("hidden");
-//     });
-// });
-
-
-
-// window.addEventListener("load", function () {
-//   setTimeout(
-//     function open(event) {
-//       document.querySelector(".popup").style.display = "block"
-//     },
-//     1000
-//   )
-// });
-
-// document.querySelector("#close").addEventListener("click", function () {
-//   document.querySelector(".popup").style.display = "none";
-// });
-
-
-
 // When the user scrolls down 200px from the top of the document, show the button
 window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -70,8 +48,6 @@ window.onscroll = function () {
       toTopButton.classList.add("hidden");
   }
 }
-
-
 
 // todfauWhen the user clicks on the button, smoothy scroll to the top of the document
 function goToTop() {
