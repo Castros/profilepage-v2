7fa3014893ac0f296c2c7d9ac98c809f5335ed1a
Cristian Castro <castrostech@gmail.com>
2024-03-15 06:36:34 -0600

    content1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/flowbite/**/*.js"
-], 
+  content: ['./*html'], 
+  content: ["./node_modules/flowbite/**/*.js"],
   
   theme: {
     screens: {
